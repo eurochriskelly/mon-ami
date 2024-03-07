f653463fbc150c8dd7fe44123be7de411c283b73
Chris Kelly <eurochriskelly@gmail.com>
2024-03-07 22:20:39 +0100

    Code cleanup
    - remove unused functions.
    - refactor qconsole only code

--- scripts/codeMirrorManipulation.js
+++
@@ -1,7 +0,0 @@
-document.addEventListener('clearCodeMirror', function(event) {
-    var cmElement = document.querySelector('.CodeMirror');
-    if (cmElement && cmElement.CodeMirror) {
-        cmElement.CodeMirror.setValue('');
-        cmElement.CodeMirror.setValue(event.detail);
-    }
-});
